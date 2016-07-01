f7b9505653201697a445b0e5589221b97bed64e2
Carolyn <crupar@gmail.com>
2016-07-01 10:05:59 -0700

    seeding with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+50.times do
+  Wiki.create!(
+    title:  Faker::Lorem.word,
+    body:   Faker::Lorem.paragraph(3)
+  )
+end
+wikis = Wiki.all
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(6)
+  )
+end
+
+
+puts "Seed finished"
+puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
